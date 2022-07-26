1f86be226fadcaf80dde6f366b0166e4db100e13
plainoldnoah <plainoldnoah@me.com>
2022-07-25 21:48:37 -0400

    Refined UI for phone, Added watch to project and fixed linking/version issues

--- SoftballApp/app/src/main/java/com/example/softballapp/MainActivity.java
+++ SoftballApp/app/src/main/java/com/example/softballapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.softballapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,23 +13,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void SecondClicked(View view) {
-        Toast toast = Toast.makeText(getApplicationContext(), "Second Clicked", Toast.LENGTH_SHORT);
-        toast.show();
-    }
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                Toast.makeText(this, "button1 pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(this, "button2 pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(this, "button3 pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button4:
+                Toast.makeText(this, "button4 pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button5:
+                Toast.makeText(this, "button5 pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button6:
+                Toast.makeText(this, "button6 pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button7:
+                Toast.makeText(this, "button7 pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button8:
+                Toast.makeText(this, "button8 pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button9:
+                Toast.makeText(this, "button9 pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button0:
+                Toast.makeText(this, "button0 pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_bonus1:
+                Toast.makeText(this, "* pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_bonus2:
+                Toast.makeText(this, "# pressed", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + view.getId());
+        }
 
-    public void ThirdClicked(View view) {
-        Toast toast = Toast.makeText(getApplicationContext(), "Third Clicked", Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    public void FirstClicked(View view) {
-        Toast toast = Toast.makeText(getApplicationContext(), "First Clicked", Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    public void HomeClicked(View view) {
-        Toast toast = Toast.makeText(getApplicationContext(), "Home Clicked", Toast.LENGTH_SHORT);
-        toast.show();
     }
 }
